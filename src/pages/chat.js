aa46e1f1ef62bac68eeacb5854193ec34b52bc77
Sokdara Cheng <chengsokdara@gmail.com>
2023-07-16 11:04:13 +0700

    test vercel env

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,21 +1,25 @@
-import React from "react";
-import Meta from "components/Meta";
+import React from 'react'
+import Meta from 'components/Meta'
 // import StreamingChat from "../components/chat/StreamingChat";
-import { requireAuth } from "../util/auth";
+import { requireAuth } from '../util/auth'
 import dynamic from 'next/dynamic'
- 
-const PorcupineChat = dynamic(() => import('../components/chat/PorcupineChat').then((mod) => mod.PorcupineChat), {
-  ssr: false,
-})
+
+const PorcupineChat = dynamic(
+  () =>
+    import('../components/chat/PorcupineChat').then((mod) => mod.PorcupineChat),
+  {
+    ssr: false,
+  }
+)
 
 function ChatPage(props) {
-    return (
-        <>
-            <Meta title="Chat" description="Chat with GPT" />
-            {/* <StreamingChat /> */}
-            <PorcupineChat />
-        </>
-    );
+  return (
+    <>
+      <Meta title="Chat" description="Chat with GPT" />
+      {/* <StreamingChat /> */}
+      <PorcupineChat />
+    </>
+  )
 }
 
-export default requireAuth(ChatPage);
+export default requireAuth(ChatPage)
