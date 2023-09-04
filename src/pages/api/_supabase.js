8a1db7dbaa7842625bc7815773d2b7f6107e0b86
marcelo.lima <marcelolima381@gmail.com>
2023-09-04 14:03:25 -0300

    Revert "deploy attempt"
    This reverts commit 6662b9ed9e115d63d07a8db7582294ee4a75b0a5.

--- src/pages/api/_supabase.js
+++ src/pages/api/_supabase.js
@@ -1,5 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
 
-const supabase = {}
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SECRET_KEY
+);
 
 module.exports = supabase;
