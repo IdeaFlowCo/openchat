5bb01a0c7807ac4ce55cd9d140cd38b888ca53fd
marcelo.lima <marcelolima381@gmail.com>
2023-09-04 13:41:04 -0300

    commented supabase code to enable deployment

--- src/util/supabase.js
+++ src/util/supabase.js
@@ -1,8 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY
-);
+// const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL,
+//   process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY
+// );
+
+const supabase = null
 
 export default supabase;
