cfd180d9e7f7a206a8724ff488f109ed8a0ba7b6
marcelo.lima <marcelolima381@gmail.com>
2023-09-04 13:50:31 -0300

    Revert "commented supabase code to enable deployment"
    This reverts commit 5bb01a0c7807ac4ce55cd9d140cd38b888ca53fd.

--- src/util/supabase.js
+++ src/util/supabase.js
@@ -1,10 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-// const supabase = createClient(
-//   process.env.NEXT_PUBLIC_SUPABASE_URL,
-//   process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY
-// );
-
-const supabase = null
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY
+);
 
 export default supabase;
