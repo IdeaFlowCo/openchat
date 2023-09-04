6662b9ed9e115d63d07a8db7582294ee4a75b0a5
marcelo.lima <marcelolima381@gmail.com>
2023-09-04 13:55:03 -0300

    deploy attempt

--- src/pages/api/_supabase.js
+++ src/pages/api/_supabase.js
@@ -1,8 +1,5 @@
 const { createClient } = require("@supabase/supabase-js");
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SECRET_KEY
-);
+const supabase = {}
 
 module.exports = supabase;
